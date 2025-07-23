e392e23ef67c3d053595563aee9bdd6db34c6627
Chafik GHOURABI <chafik.ghourabi@satoripop.com>
2025-07-24 00:39:05 +0100

    feat: add custom favicon and update portfolio metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,36 @@
-import type {Metadata} from 'next';
-import './globals.css';
-import { Toaster } from "@/components/ui/toaster"
+import type { Metadata } from "next";
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
-  title: 'ReactNativePro Portfolio',
-  description: 'Portfolio of a Senior React Native Developer',
+	title: "Chafik GHOURABI Portfolio",
+	description: "Portfolio of a Mobile React Native Developer",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" className="!scroll-smooth">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Poppins:wght@600;700&display=swap" rel="stylesheet" />
-      </head>
-      <body className="font-body antialiased">
-        {children}
-        <Toaster />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className="!scroll-smooth">
+			<head>
+				<link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link
+					rel="preconnect"
+					href="https://fonts.gstatic.com"
+					crossOrigin="anonymous"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&family=Poppins:wght@600;700&display=swap"
+					rel="stylesheet"
+				/>
+			</head>
+			<body className="font-body antialiased">
+				{children}
+				<Toaster />
+			</body>
+		</html>
+	);
 }
